b594a88c573ab3b8ddbd663f2b42d617f942f267
jystella17 <jystella21617@gmail.com>
2020-04-05 20:24:46 +0900

    404 error

--- django/ceos/polls/views.py
+++ django/ceos/polls/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 from django.template import loader
 
@@ -12,11 +13,12 @@ def index(request):
         'latest_question_list' : latest_question_list
     }
     output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html',context)
 
 # Create your views here.
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
